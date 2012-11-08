b02291442af787cad4938b8388f4d424eeae2617
Benhard Risando <benhard.simatupang@sixceed.com>
2012-11-08 11:20:10 +0700

    create search page

--- app/friendships/admin.py
+++ app/friendships/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin 
+from django.contrib import admin
 from app.friendships.models import User
 
 
---
+++ app/friendships/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+def search(request):
+    looks = User.objects.all()
+    # paginator = Paginator(look, 2)
+
+    # page = request.GET.get('page')
+    # try:
+    #     look_for = paginator.page(page)
+    # except PageNotAnInteger:
+    #     # If page is not an integer, deliver first page.
+    #     look_for = paginator.page(1)
+    # except EmptyPage:
+    #     # If page is out of range (e.g. 9999), deliver last page of results.
+    #     look_for = paginator.page(paginator.num_pages)
+    variables = RequestContext(request, {
+        'looks': looks
+        })
+    return render_to_response('search.html', variables)
