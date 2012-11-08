70492648d7e8a70ff4df07b52aababcb029b2986
Ronald Tampubolon <ronald.tampubolon@sixceed.com>
2012-11-08 17:29:28 +0700

    using django-friendship to add/remove friends

--- app/panel/models.py
+++ app/panel/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    User = models.OneToOneField(User)
+    avatar = models.ImageField(upload_to='userprofile/')
--- schoolnetwork/settings.py
+++ schoolnetwork/settings.py
@@ -158,7 +158,7 @@ INSTALLED_APPS = (
     #'allauth.socialaccount.providers.persona',
     #'allauth.socialaccount.providers.soundcloud',
     'postman',
-    'relationships',
+    'friendship',
     'pagination'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
@@ -166,19 +166,19 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 )
 
-POSTMAN_DISALLOW_ANONYMOUS = True # default is False
+POSTMAN_DISALLOW_ANONYMOUS = True  # default is False
 # POSTMAN_DISALLOW_MULTIRECIPIENTS = True # default is False
 # POSTMAN_DISALLOW_COPIES_ON_REPLY = True # default is False
 # POSTMAN_DISABLE_USER_EMAILING = True # default is False
-POSTMAN_AUTO_MODERATE_AS = True # default is None
-POSTMAN_NOTIFIER_APP = 'notification' # default is 'notification'
-POSTMAN_MAILER_APP = 'mailer' # default is 'mailer'
+POSTMAN_AUTO_MODERATE_AS = True  # default is None
+POSTMAN_NOTIFIER_APP = 'notification'  # default is 'notification'
+POSTMAN_MAILER_APP = 'mailer'  # default is 'mailer'
 POSTMAN_AUTOCOMPLETER_APP = {
-     'name': '', # default is 'ajax_select'
+     'name': '',  # default is 'ajax_select'
     # 'field': '', # default is 'AutoCompleteField'
     # 'arg_name': '', # default is 'channel'
     # 'arg_default': 'postman_friends', # no default, mandatory to enable the feature
- } # default is {}
+ }  # default is {}
 
 
 LOGGING = {
@@ -215,8 +215,6 @@ ACCOUNT_ACTIVATION_DAYS = 7
 DEFAULT_FROM_EMAIL = 'SOCIAL KID SYSTEM <benhardrisando@gmail.com>'
 
 DEFAULT_CONTENT_EMAIL = "Thank you for your submitted questions. Our staff has received your questions and will be replied shortly"
-
-
 
 try:
     from local_settings import *
