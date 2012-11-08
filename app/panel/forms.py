21e8ffe29ea538a003f29ca744d8136b6924552c
Ronald Tampubolon <ronald.tampubolon@sixceed.com>
2012-11-08 11:55:42 +0700

    change registration using django-allauth

--- app/panel/forms.py
+++ app/panel/forms.py
@@ -1,24 +1,19 @@
 from django import forms
-from registration.forms import RegistrationForm
-from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth.models import User
 #from registration.signals import user_registered
 
 
-class UserRegistrationForm(RegistrationForm):
-    first_name = forms.CharField(label=_('First Name'), max_length=30)
-    last_name = forms.CharField(label=_('Last Name'), max_length=30)
+class SignupForm(forms.Form):
+    first_name = forms.CharField(max_length=200)
+    last_name = forms.CharField(max_length=200)
 
     def __init__(self, *args, **kwargs):
-        super(RegistrationForm, self).__init__(*args, **kwargs)
+        super(SignupForm, self).__init__(*args, **kwargs)
         self.fields.keyOrder = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
 
-    def clean_username(self):
-        try:
-            User.objects.get(username__iexact=self.cleaned_data['username'])
-        except User.DoesNotExist:
-            return self.cleaned_data['username']
-        raise forms.ValidationError(_("A user with that username already exists."))
+    def save(self, user):
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
 
     # def clean_email(self):
     #     data = self.cleaned_data
--- schoolnetwork/urls.py
+++ schoolnetwork/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from app.panel.forms import UserRegistrationForm
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -20,17 +19,18 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('app.panel.urls', namespace='panel')),
     url(r'', include('app.friendships.urls', namespace='friendships')),
-    url(r'^accounts/register/$', 'registration.views.register', {
-        'backend': 'registration.backends.default.DefaultBackend',
-        'form_class': UserRegistrationForm}, name='registration_register'),
+       (r'^accounts/', include('allauth.urls')),
+    # url(r'^accounts/signup/$', 'registration.views.register', {
+    #     'backend': 'registration.backends.default.DefaultBackend',
+    #     'form_class': UserRegistrationForm}, name='registration_register'),
 
-    url(r'^accounts/', include('registration.backends.default.urls')),
-    (r'^accounts/password/reset/$', 'django.contrib.auth.views.password_reset',
-        {'post_reset_redirect': '/accounts/password/reset/done/'}),
-    (r'^accounts/password/reset/done/$', 'django.contrib.auth.views.password_reset_done'),
-    (r'^accounts/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm',
-        {'post_reset_redirect': '/accounts/password/done/'}),
-    (r'^accounts/password/done/$', 'django.contrib.auth.views.password_reset_complete'),
+    # url(r'^accounts/', include('registration.backends.default.urls')),
+    # (r'^accounts/password/reset/$', 'django.contrib.auth.views.password_reset',
+    #     {'post_reset_redirect': '/accounts/password/reset/done/'}),
+    # (r'^accounts/password/reset/done/$', 'django.contrib.auth.views.password_reset_done'),
+    # (r'^accounts/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm',
+    #     {'post_reset_redirect': '/accounts/password/done/'}),
+    # (r'^accounts/password/done/$', 'django.contrib.auth.views.password_reset_complete'),
 )
 
 if settings.DEBUG:
