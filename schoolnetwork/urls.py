4dd1b4da2922f890b75cb90761b90675fcc7ff1c
Reza Muhammad <rezmuh@sixceed.com>
2012-11-12 19:35:53 +0700

    remove junk

--- schoolnetwork/urls.py
+++ schoolnetwork/urls.py
@@ -1,45 +1,17 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-#from organizations.backends import invitation_backend
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'nutrition.views.home', name='home'),
-    # url(r'^nutrition/', include('nutrition.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^messages/', include('postman.urls')),
-    #url(r'^accounts/signup/$', 'app.panel.views.signup', name='account_signup'),
     url(r'^relationships/', include('friendship.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('app.panel.urls', namespace='panel')),
+    url(r'^accounts/', include('allauth.urls')),
+    url(r'^avatar/', include('avatar.urls')),
+    url(r'^(?P<username>[-\.\w]+)/', include('app.userprofile.urls', namespace='userprofile')),
     url(r'', include('app.connections.urls', namespace='connections')),
-    url(r'', include('app.userprofile.urls', namespace='userprofile')),
-    url(r'', include('app.friendships.urls', namespace='friendships')),
-       (r'^accounts/', include('allauth.urls')),
-       (r'^avatar/', include('avatar.urls')),
-    #url(r'^groups/', include('organizations.urls')),
-    #url(r'^invitations/', include(invitation_backend().get_urls())),
-
-
-    # url(r'^accounts/signup/$', 'registration.views.register', {
-    #     'backend': 'registration.backends.default.DefaultBackend',
-    #     'form_class': UserRegistrationForm}, name='registration_register'),
-
-    # url(r'^accounts/', include('registration.backends.default.urls')),
-    # (r'^accounts/password/reset/$', 'django.contrib.auth.views.password_reset',
-    #     {'post_reset_redirect': '/accounts/password/reset/done/'}),
-    # (r'^accounts/password/reset/done/$', 'django.contrib.auth.views.password_reset_done'),
-    # (r'^accounts/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm',
-    #     {'post_reset_redirect': '/accounts/password/done/'}),
-    # (r'^accounts/password/done/$', 'django.contrib.auth.views.password_reset_complete'),
 )
 
 if settings.DEBUG:
