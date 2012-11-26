aed729b68f1cf8f94b858d6d24d124ddd6c2b254
Reza Muhammad <rezmuh@sixceed.com>
2012-11-26 23:30:20 +0700

    oops, added missing files

---
+++ app/academy/templatetags/university_tags.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def is_school_admins_for(user, university):
+    return user.academy_roles.filter(university=university, role=3).exists()
+
+
+@register.filter
+def is_professor_for(user, university):
+    return user.academy_roles.filter(university=university, role=3).exists()
