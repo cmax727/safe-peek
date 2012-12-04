e4d65bead663ce4d62b14db07c017a54e82bcf17
Reza Muhammad <rezmuh@sixceed.com>
2012-12-04 22:43:53 +0700

    do not show "add as a friend" if a user has sent a friendship request to that person

--- app/connections/templatetags/connections_tags.py
+++ app/connections/templatetags/connections_tags.py
@@ -3,8 +3,11 @@ from friendship.models import Friend
 
 register = template.Library()
 
+
 @register.filter
 def is_friend_with(request_user, other_user):
     if request_user != other_user:
-        return Friend.objects.are_friends(request_user, other_user)
+        friends = Friend.objects.are_friends(request_user, other_user)
+        sent_requests = request_user.friendship_requests_sent.filter(to_user=other_user)
+        return friends or len(sent_requests) > 0
     return True
