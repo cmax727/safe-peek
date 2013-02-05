83aabfdb9e4cf1b452fbe70b7e46b0f21a4fc318
Sidorenko Vladimir <yoyavova@gmail.com>
2013-02-05 18:33:54 +0200

    fixed email

--- schoolnetwork/settings.py
+++ schoolnetwork/settings.py
@@ -259,7 +259,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_HOST_PASSWORD = 'testtest'
-SERVER_EMAIL = DEFAULT_FROM_EMAIL = EMAIL_HOST_USER = 'test@looksmi.com.
+SERVER_EMAIL = DEFAULT_FROM_EMAIL = EMAIL_HOST_USER = 'test@looksmi.com.ua'
 EMAIL_PORT = 25
 
 
